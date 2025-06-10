ba490dc459a1ab23549a79b91eb430b411e7e2b4
CAndres438 <candresortiz123@gmail.com>
2025-06-10 10:03:16 -0500

    🔧 chore: add origin in system application

--- src/main/java/com/nexos/inventory_system/InventorySystemApplication.java
+++ src/main/java/com/nexos/inventory_system/InventorySystemApplication.java
@@ -1,13 +1,34 @@
 package com.nexos.inventory_system;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class InventorySystemApplication {
 
+	@Value("${cores.allowed-origin}")
+	private String allowedOrigin;
+
 	public static void main(String[] args) {
 		SpringApplication.run(InventorySystemApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfig() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins(allowedOrigin)
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+			}
+		};
 	}
 
 }
